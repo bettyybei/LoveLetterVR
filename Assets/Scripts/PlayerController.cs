3b460a04cee5e215a184f968088fd716f34ab222
Betty Bei <bettybei96@gmail.com>
2017-11-01 22:58:52 -0400

    add more to player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,15 +6,19 @@ using FRL.IO;
 
 public class PlayerController : MonoBehaviour, IGlobalTriggerPressDownHandler {
 
-    enum State { Dead, Guard, Priest, Baron, Handmaid, Prince, King, Countess, Princess }
+    public enum State { Dead, Guard, Priest, Baron, Handmaid, Prince, King, Countess, Princess }
+
     bool immune = false;
+    PlayerController chosenOtherPlayer;
 
     State current;
     State dismiss;
 
+    bool isChoosingOtherPlayer = false;
+
 	// Use this for initialization
 	void Start () {
-		
+	
 	}
 	
 	// Update is called once per frame
@@ -22,6 +26,7 @@ public class PlayerController : MonoBehaviour, IGlobalTriggerPressDownHandler {
 		
 	}
 
+    #region General Player Methods
     void StartTurn(State next)
     {
         dismiss = next;
@@ -36,6 +41,37 @@ public class PlayerController : MonoBehaviour, IGlobalTriggerPressDownHandler {
         }
     }
 
+    void Dismiss()
+    {
+        switch (dismiss)
+        {
+            case State.Guard:
+                State guess = ChooseOtherPlayerState();
+                GuardAttack(guess);
+                break;
+            case State.Priest:
+                PriestReveal();
+                break;
+            case State.Baron:
+                BaronBattle();
+                break;
+            case State.Handmaid:
+                immune = true;
+                break;
+            case State.Prince:
+                PrinceForceDiscard();
+                break;
+            case State.King:
+                KingTradeHands();
+                break;
+            case State.Countess:
+                break;
+            case State.Princess:
+                Die();
+                break;
+        }
+    }
+
     State ChooseStateToDismiss()
     {
         if (true) //TODO
@@ -47,55 +83,115 @@ public class PlayerController : MonoBehaviour, IGlobalTriggerPressDownHandler {
         dismiss = temp;
     }
 
-    void Dismiss()
-    {
-        switch (dismiss)
-        {
-            case State.Guard:
-                State state = ChooseOtherPlayerState();
-                ChooseOtherPlayer();
-                break;
-            case State.Priest:
-                ChooseOtherPlayer();
-                //Reveal Other Player();
-                break;
-            case State.Baron:
-                break;
-            case State.Handmaid:
-                immune = true;
-                break;
-            case State.Prince:
-                ChooseOtherPlayer();
-                break;
-            case State.King:
-                ChooseOtherPlayer();
-                break;
-            case State.Countess:
-                break;
-            case State.Princess:
-                break;
-        }
-    }
-
-    void ChooseOtherPlayer()
-    {
-
-    }
-
     State ChooseOtherPlayerState()
     {
         return State.Dead;
     }
 
+    void Die()
+    {
+        current = State.Dead;
+    }
+    #endregion
+
+
+    #region Dismissal Actions
+    void GuardAttack(State guess)
+    {
+        chosenOtherPlayer = null;
+        isChoosingOtherPlayer = true;
+        while (chosenOtherPlayer != null)
+        {
+            //wait for player to choose other player
+        }
+        isChoosingOtherPlayer = false;
+        if (chosenOtherPlayer.current == guess)
+        {
+            //success
+            chosenOtherPlayer.Die();
+        }
+        else
+        {
+            //better luck next time
+        }
+    }
+
+    void PriestReveal()
+    {
+        chosenOtherPlayer = null;
+        isChoosingOtherPlayer = true;
+        while (chosenOtherPlayer != null)
+        {
+            //wait for player to choose other player
+        }
+        isChoosingOtherPlayer = false;
+        //Reveal other player
+    }
+
+    void BaronBattle()
+    {
+        chosenOtherPlayer = null;
+        isChoosingOtherPlayer = true;
+        while (chosenOtherPlayer != null)
+        {
+            //wait for player to choose other player
+        }
+        isChoosingOtherPlayer = false;
+        if (chosenOtherPlayer.current < current)
+        {
+            //success
+            chosenOtherPlayer.Die();
+        }
+        else if (chosenOtherPlayer.current > current)
+        {
+            //fail
+            Die();
+        }
+        else
+        {
+            //tie
+        }
+    }
+
+    void PrinceForceDiscard()
+    {
+        chosenOtherPlayer = null;
+        isChoosingOtherPlayer = true;
+        while (chosenOtherPlayer != null)
+        {
+            //wait for player to choose other player
+        }
+        isChoosingOtherPlayer = false;
+        // new state for other player
+    }
+
+    void KingTradeHands()
+    {
+        chosenOtherPlayer = null;
+        isChoosingOtherPlayer = true;
+        while (chosenOtherPlayer != null)
+        {
+            //wait for player to choose other player
+        }
+        isChoosingOtherPlayer = false;
+        State temp = current;
+        current = chosenOtherPlayer.current;
+        chosenOtherPlayer.current = temp;
+    }
+    #endregion
+
+    #region Vive Controller Methods
     void IGlobalTriggerPressDownHandler.OnGlobalTriggerPressDown(VREventData eventData)
     {
-        PlayerController opc = eventData.currentRaycast.GetComponent<PlayerController>();
-        if (opc != null)
+        PlayerController otherPlayerController = eventData.currentRaycast.GetComponent<PlayerController>();
+        if (isChoosingOtherPlayer == true && otherPlayerController != null)
         {
+            chosenOtherPlayer = otherPlayerController;
             //We're pointing at a PlayerController! Do something.
         } else
         {
             //We're not pointing at a PlayerController.
         }
     }
+    #endregion
 }
