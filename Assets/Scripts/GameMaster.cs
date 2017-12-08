bf29a6965bc8fd552bb63120b14bee481a35561b
Betty Bei <bettybei96@gmail.com>
2017-12-08 15:12:45 -0500

    detabify

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -3,167 +3,167 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameMaster: MonoBehaviour {
-	public enum State { Dead, Guard, Priest, Baron, Handmaid, Prince, King, Countess, Princess }
+    public enum State { Dead, Guard, Priest, Baron, Handmaid, Prince, King, Countess, Princess }
 
-	public PlayerController player1;
-	public PlayerController player2;
+    public PlayerController player1;
+    public PlayerController player2;
 
-	public GameObject stateMenuObject;
-	public GameObject twoStateMenuObject;
-	StateController stateCard1;
-	StateController stateCard2;
+    public GameObject stateMenuObject;
+    public GameObject twoStateMenuObject;
+    StateController stateCard1;
+    StateController stateCard2;
 
-	Random _Random = new Random();
+    Random _Random = new Random();
 
-	const string _GameStatusWin = "Game over. You win!";
-	const string _GameStatusLose = "Game over. You lost.";
-	const string _GameStatusTie = "Game over. It was a tie!";
+    const string _GameStatusWin = "Game over. You win!";
+    const string _GameStatusLose = "Game over. You lost.";
+    const string _GameStatusTie = "Game over. It was a tie!";
 
-	PlayerController[] players;
+    PlayerController[] players;
 
-	int currentPlayerCount;
+    int currentPlayerCount;
 
-	// synced objects : deck, nextStateIdx, playerStates, currentPlayerIdx
-	public State[] deck = new State[] {
-		State.Guard, State.Guard, State.Guard, State.Guard, State.Guard, 
-		State.Priest, State.Priest,
-		State.Baron, State.Baron,
-		State.Handmaid, State.Handmaid,
-		State.Prince, State.Prince,
-		State.King,
-		State.Countess,
-		State.Princess
-	};
-		
-	public int nextStateIdx = 1; // Skips card at index 0 because one card is taken out.
-	public State[] playerStates; // playerStates will always be max # of players
-	public int currentPlayerIdx = 0;
+    // synced objects : deck, nextStateIdx, playerStates, currentPlayerIdx
+    public State[] deck = new State[] {
+        State.Guard, State.Guard, State.Guard, State.Guard, State.Guard, 
+        State.Priest, State.Priest,
+        State.Baron, State.Baron,
+        State.Handmaid, State.Handmaid,
+        State.Prince, State.Prince,
+        State.King,
+        State.Countess,
+        State.Princess
+    };
+        
+    public int nextStateIdx = 1; // Skips card at index 0 because one card is taken out.
+    public State[] playerStates; // playerStates will always be max # of players
+    public int currentPlayerIdx = 0;
 
-	void Start () {
-		// Populate State Menu State Controllers
-		StateController[] menuStateControllers = stateMenuObject.GetComponentsInChildren<StateController>();
-		for (int i = 0; i < menuStateControllers.Length; i++) {
-			menuStateControllers[i].SetState((State)(i + 1));
-		}
-		StateController[] cardStateControllers = twoStateMenuObject.GetComponentsInChildren<StateController>();
-		stateCard1 = cardStateControllers[0];
-		stateCard2 = cardStateControllers[1];
+    void Start () {
+        // Populate State Menu State Controllers
+        StateController[] menuStateControllers = stateMenuObject.GetComponentsInChildren<StateController>();
+        for (int i = 0; i < menuStateControllers.Length; i++) {
+            menuStateControllers[i].SetState((State)(i + 1));
+        }
+        StateController[] cardStateControllers = twoStateMenuObject.GetComponentsInChildren<StateController>();
+        stateCard1 = cardStateControllers[0];
+        stateCard2 = cardStateControllers[1];
 
-		deck = ShuffleDeck(deck);
+        deck = ShuffleDeck(deck);
 
-		players = new PlayerController[] {
-			player1, player2
-		};
+        players = new PlayerController[] {
+            player1, player2
+        };
 
-		currentPlayerCount = players.Length;
-		playerStates = new State[4];
+        currentPlayerCount = players.Length;
+        playerStates = new State[4];
 
-		// Picking initial cards
-		for (int i = 0; i < currentPlayerCount; i++) {
-			State s = deck[nextStateIdx++];
-			players[i].SetState(s);
-			playerStates[i] = s;
-		}
+        // Picking initial cards
+        for (int i = 0; i < currentPlayerCount; i++) {
+            State s = deck[nextStateIdx++];
+            players[i].SetState(s);
+            playerStates[i] = s;
+        }
 
-		// Start first player's turn
-		StartPlayersTurn(deck[nextStateIdx++], deck[nextStateIdx]);
-	}
+        // Start first player's turn
+        StartPlayersTurn(deck[nextStateIdx++], deck[nextStateIdx]);
+    }
 
-	void Update () {
-		PlayerController currentPlayer = players[currentPlayerIdx];
+    void Update () {
+        PlayerController currentPlayer = players[currentPlayerIdx];
 
-		if (currentPlayer.IsChoosingOwnState != twoStateMenuObject.activeSelf) {
-			twoStateMenuObject.SetActive(currentPlayer.IsChoosingOwnState);
-		} else if (currentPlayer.IsChoosingMenuState != stateMenuObject.activeSelf) {
-			stateMenuObject.SetActive(currentPlayer.IsChoosingMenuState);
-		}
+        if (currentPlayer.IsChoosingOwnState != twoStateMenuObject.activeSelf) {
+            twoStateMenuObject.SetActive(currentPlayer.IsChoosingOwnState);
+        } else if (currentPlayer.IsChoosingMenuState != stateMenuObject.activeSelf) {
+            stateMenuObject.SetActive(currentPlayer.IsChoosingMenuState);
+        }
 
-		if (nextStateIdx == 16) {
-			// Game over. Calculate winner(s)
-			int winnerIdx = 0;
-			int tie1 = -1; 
-			int tie2 = -1; // rare, but a 3 way tie is possible
-			for (int i = 1; i < players.Length; i++) {
-				State state = players[i].CurrentState;
-				State maxState = players[winnerIdx].CurrentState;
-				if (state > maxState) {
-					winnerIdx = i;
-					tie1 = -1;
-					tie2 = -1;
-				}
-				if (state == maxState) {
-					// save tied indices
-					if (tie1 > 0)
-						tie2 = i;
-					else
-						tie1 = i;
-				}
-			}
+        if (nextStateIdx == 16) {
+            // Game over. Calculate winner(s)
+            int winnerIdx = 0;
+            int tie1 = -1; 
+            int tie2 = -1; // rare, but a 3 way tie is possible
+            for (int i = 1; i < players.Length; i++) {
+                State state = players[i].CurrentState;
+                State maxState = players[winnerIdx].CurrentState;
+                if (state > maxState) {
+                    winnerIdx = i;
+                    tie1 = -1;
+                    tie2 = -1;
+                }
+                if (state == maxState) {
+                    // save tied indices
+                    if (tie1 > 0)
+                        tie2 = i;
+                    else
+                        tie1 = i;
+                }
+            }
 
-			// Set Game Status Text
-			for (int i = 0; i < players.Length; i++) {
-				if (i == winnerIdx || i == tie1 || i == tie2) {
-					if (tie1 > 0)
-						players[i].gameStatusTextObject.text = _GameStatusTie;
-					else
-						players[i].gameStatusTextObject.text = _GameStatusWin;
-				}
-				else 
-					players[i].gameStatusTextObject.text = _GameStatusLose;
-			}
-			nextStateIdx++;
-		}
+            // Set Game Status Text
+            for (int i = 0; i < players.Length; i++) {
+                if (i == winnerIdx || i == tie1 || i == tie2) {
+                    if (tie1 > 0)
+                        players[i].gameStatusTextObject.text = _GameStatusTie;
+                    else
+                        players[i].gameStatusTextObject.text = _GameStatusWin;
+                }
+                else 
+                    players[i].gameStatusTextObject.text = _GameStatusLose;
+            }
+            nextStateIdx++;
+        }
 
-		else if (nextStateIdx < 16 && !currentPlayer.IsDoingTurn) {
-			// sync up and count player states after the end of each turn
-			currentPlayerCount = 0;
-			for (int i = 0; i < players.Length; i++) {
-				State s = players[i].CurrentState;
-				if (s != State.Dead)
-					currentPlayerCount++;
-				if (playerStates[i] != s) 
-					playerStates[i] = s;
-			}
-				
-			if (currentPlayer.UsedNextState) {
-				nextStateIdx++;
-				currentPlayer.UsedNextState = false;
-			}
+        else if (nextStateIdx < 16 && !currentPlayer.IsDoingTurn) {
+            // sync up and count player states after the end of each turn
+            currentPlayerCount = 0;
+            for (int i = 0; i < players.Length; i++) {
+                State s = players[i].CurrentState;
+                if (s != State.Dead)
+                    currentPlayerCount++;
+                if (playerStates[i] != s) 
+                    playerStates[i] = s;
+            }
+                
+            if (currentPlayer.UsedNextState) {
+                nextStateIdx++;
+                currentPlayer.UsedNextState = false;
+            }
 
-			if (currentPlayerCount == 1) {
-				nextStateIdx = 16; //this ends the game in the next Update call
-				return;
-			}
+            if (currentPlayerCount == 1) {
+                nextStateIdx = 16; //this ends the game in the next Update call
+                return;
+            }
 
-			if (currentPlayerCount > 1) {
-				do {
-					currentPlayerIdx = (currentPlayerIdx + 1) % players.Length;
-					currentPlayer = players[currentPlayerIdx];
-				} while(currentPlayer.CurrentState == State.Dead);
-			}
+            if (currentPlayerCount > 1) {
+                do {
+                    currentPlayerIdx = (currentPlayerIdx + 1) % players.Length;
+                    currentPlayer = players[currentPlayerIdx];
+                } while(currentPlayer.CurrentState == State.Dead);
+            }
 
-			if (currentPlayerCount > 1 && currentPlayer.CurrentState != State.Dead) {
-				StartPlayersTurn(deck[nextStateIdx++], deck[nextStateIdx]);
-			}
-		}
-	}
+            if (currentPlayerCount > 1 && currentPlayer.CurrentState != State.Dead) {
+                StartPlayersTurn(deck[nextStateIdx++], deck[nextStateIdx]);
+            }
+        }
+    }
 
-	void StartPlayersTurn(State next, State nextnext) {
-		PlayerController currentPlayer = players[currentPlayerIdx];
-		State previous = currentPlayer.CurrentState;
-		currentPlayer.IsDoingTurn = true;
-		stateCard1.SetState(previous);
-		stateCard2.SetState(next);
-		currentPlayer.StartTurn(next, nextnext);
-	}
+    void StartPlayersTurn(State next, State nextnext) {
+        PlayerController currentPlayer = players[currentPlayerIdx];
+        State previous = currentPlayer.CurrentState;
+        currentPlayer.IsDoingTurn = true;
+        stateCard1.SetState(previous);
+        stateCard2.SetState(next);
+        currentPlayer.StartTurn(next, nextnext);
+    }
 
-	State[] ShuffleDeck(State[] deck) {
-		for (int i = deck.Length-1; i > 0; i--) {
-			int j = Random.Range(0, i);
-			State temp = deck[i];
-			deck[i] = deck[j];
-			deck[j] = temp;
-		}
-		return deck;
-	}
+    State[] ShuffleDeck(State[] deck) {
+        for (int i = deck.Length-1; i > 0; i--) {
+            int j = Random.Range(0, i);
+            State temp = deck[i];
+            deck[i] = deck[j];
+            deck[j] = temp;
+        }
+        return deck;
+    }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,235 +7,235 @@ using State = GameMaster.State;
 
 public class PlayerController : MonoBehaviour, IGlobalTriggerPressDownHandler {
 
-	public GameObject pointerObject;
-	public TextMesh stateTextObject;
-	public TextMesh gameStatusTextObject;
+    public GameObject pointerObject;
+    public TextMesh stateTextObject;
+    public TextMesh gameStatusTextObject;
 
     PlayerController chosenOtherPlayer;
-	StateController chosenStateController;
+    StateController chosenStateController;
 
-	public State CurrentState { get; private set; }
-	private State DismissState { get; set; }
-	private State NextStateInDeck { get; set; }
+    public State CurrentState { get; private set; }
+    private State DismissState { get; set; }
+    private State NextStateInDeck { get; set; }
 
-	public bool IsDoingTurn { get; set; }
-	public bool IsChoosingOwnState { get; private set; }
-	public bool IsChoosingMenuState { get; private set; }
-	public bool UsedNextState { get; set; }
+    public bool IsDoingTurn { get; set; }
+    public bool IsChoosingOwnState { get; private set; }
+    public bool IsChoosingMenuState { get; private set; }
+    public bool UsedNextState { get; set; }
 
-	private bool IsChoosingOtherPlayer { get; set; }
-	private bool AllowChooseSelf { get; set; }
-	private bool Immune { get; set; }
+    private bool IsChoosingOtherPlayer { get; set; }
+    private bool AllowChooseSelf { get; set; }
+    private bool Immune { get; set; }
 
-	void Update () {
-		bool pointerEnabled = IsChoosingOtherPlayer || IsChoosingOwnState || IsChoosingMenuState;
+    void Update () {
+        bool pointerEnabled = IsChoosingOtherPlayer || IsChoosingOwnState || IsChoosingMenuState;
         if (pointerEnabled != pointerObject.activeSelf)
         {
             pointerObject.SetActive(pointerEnabled);
         }
-	}
+    }
 
     #region General Player Methods
-	public void SetState(State s) {
-		CurrentState = s;
-		stateTextObject.text = "Currently Holding: " + s.ToString();
-	}
+    public void SetState(State s) {
+        CurrentState = s;
+        stateTextObject.text = "Currently Holding: " + s.ToString();
+    }
 
-	public void StartTurn(State next, State nextnext) //nextnext is needed for PrinceForceDiscard()
+    public void StartTurn(State next, State nextnext) //nextnext is needed for PrinceForceDiscard()
     {
-		if (Immune == true) Immune = false;
-		DismissState = next;
-		NextStateInDeck = nextnext;
-		StartCoroutine(ChooseDismiss());
+        if (Immune == true) Immune = false;
+        DismissState = next;
+        NextStateInDeck = nextnext;
+        StartCoroutine(ChooseDismiss());
     }
 
     IEnumerator ChooseDismiss()
     {
-		if (DismissState == State.Countess && (CurrentState == State.Prince || CurrentState == State.King) )
-		{
-			// Skip choosing state to dismiss
-			gameStatusTextObject.text = "The Countess is dismissed because you have a Prince or King";
-		}
-		else
-		{
-			gameStatusTextObject.text = "Choose a card you would like to discard and enact its power";
-			yield return StartCoroutine(ChooseStateToDismiss());
-		}
-			
+        if (DismissState == State.Countess && (CurrentState == State.Prince || CurrentState == State.King) )
+        {
+            // Skip choosing state to dismiss
+            gameStatusTextObject.text = "The Countess is dismissed because you have a Prince or King";
+        }
+        else
+        {
+            gameStatusTextObject.text = "Choose a card you would like to discard and enact its power";
+            yield return StartCoroutine(ChooseStateToDismiss());
+        }
+            
         switch (DismissState)
         {
-			case State.Guard:
-				yield return StartCoroutine(ChooseOtherPlayerState());
-				yield return StartCoroutine(GuardAttack());
+            case State.Guard:
+                yield return StartCoroutine(ChooseOtherPlayerState());
+                yield return StartCoroutine(GuardAttack());
                 break;
-			case State.Priest:
-				yield return StartCoroutine(PriestReveal());
+            case State.Priest:
+                yield return StartCoroutine(PriestReveal());
                 break;
             case State.Baron:
-				yield return StartCoroutine(BaronBattle());
+                yield return StartCoroutine(BaronBattle());
                 break;
-			case State.Handmaid:
-				gameStatusTextObject.text = "You are immune until your next turn";
+            case State.Handmaid:
+                gameStatusTextObject.text = "You are immune until your next turn";
                 Immune = true;
                 break;
-			case State.Prince:
-				yield return StartCoroutine(PrinceForceDiscard());
+            case State.Prince:
+                yield return StartCoroutine(PrinceForceDiscard());
                 break;
             case State.King:
-				yield return StartCoroutine(KingTradeHands());
+                yield return StartCoroutine(KingTradeHands());
                 break;
-			case State.Countess:
-				gameStatusTextObject.text = "You dismissed the Countess. No powers are enacted";
+            case State.Countess:
+                gameStatusTextObject.text = "You dismissed the Countess. No powers are enacted";
                 break;
             case State.Princess:
                 Die();
                 break;
         }
-		IsDoingTurn = false;
+        IsDoingTurn = false;
     }
 
     IEnumerator ChooseStateToDismiss()
     {
-		chosenStateController = null;
-		IsChoosingOwnState = true;
-		while (chosenStateController == null) {
-			// wait for player to choose state to dismiss
-			yield return null;
-		}
-		IsChoosingOwnState = false;
-		if (chosenStateController.GetState() == CurrentState) // If they want to dismiss their current state
+        chosenStateController = null;
+        IsChoosingOwnState = true;
+        while (chosenStateController == null) {
+            // wait for player to choose state to dismiss
+            yield return null;
+        }
+        IsChoosingOwnState = false;
+        if (chosenStateController.GetState() == CurrentState) // If they want to dismiss their current state
         {
-			State temp = CurrentState;
-			SetState(DismissState);
-			DismissState = temp;
+            State temp = CurrentState;
+            SetState(DismissState);
+            DismissState = temp;
         }
     }
 
     IEnumerator ChooseOtherPlayerState()
     {
-		gameStatusTextObject.text = "Choose a state you believe another player has";
-		chosenStateController = null;
-		IsChoosingMenuState = true;
-		while (chosenStateController == null) {
-			// wait for player to choose state from menu
-			yield return null;
-		}
-		IsChoosingMenuState = false;
+        gameStatusTextObject.text = "Choose a state you believe another player has";
+        chosenStateController = null;
+        IsChoosingMenuState = true;
+        while (chosenStateController == null) {
+            // wait for player to choose state from menu
+            yield return null;
+        }
+        IsChoosingMenuState = false;
     }
 
-	IEnumerator ChooseOtherPlayer() {
-		chosenOtherPlayer = null;
-		IsChoosingOtherPlayer = true;
-		while (chosenOtherPlayer == null)
-		{
-			//wait for player to choose other player
-			yield return null;
-		}
-		IsChoosingOtherPlayer = false;
-	}
+    IEnumerator ChooseOtherPlayer() {
+        chosenOtherPlayer = null;
+        IsChoosingOtherPlayer = true;
+        while (chosenOtherPlayer == null)
+        {
+            //wait for player to choose other player
+            yield return null;
+        }
+        IsChoosingOtherPlayer = false;
+    }
 
     void Die()
     {
-		gameStatusTextObject.text = "You died! Game over. You lost.";
-		SetState (State.Dead);
+        gameStatusTextObject.text = "You died! Game over. You lost.";
+        SetState (State.Dead);
     }
     #endregion
 
 
     #region Dismissal Actions
 
-	IEnumerator GuardAttack() {
-		State guess = chosenStateController.GetState();
+    IEnumerator GuardAttack() {
+        State guess = chosenStateController.GetState();
 
-		gameStatusTextObject.text = "Choose another player you want to attack";
-		yield return StartCoroutine(ChooseOtherPlayer());
-		if (chosenOtherPlayer.CurrentState == guess)
-		{
-			//success
-			gameStatusTextObject.text = "You guessed correct! " + chosenOtherPlayer.name + " is dead.";
-			chosenOtherPlayer.Die();
-		}
-		else
-		{
-			//fail
-			gameStatusTextObject.text = "You guessed incorrectly. " + chosenOtherPlayer.name + " is still in the game.";
-		} 
-	}
+        gameStatusTextObject.text = "Choose another player you want to attack";
+        yield return StartCoroutine(ChooseOtherPlayer());
+        if (chosenOtherPlayer.CurrentState == guess)
+        {
+            //success
+            gameStatusTextObject.text = "You guessed correct! " + chosenOtherPlayer.name + " is dead.";
+            chosenOtherPlayer.Die();
+        }
+        else
+        {
+            //fail
+            gameStatusTextObject.text = "You guessed incorrectly. " + chosenOtherPlayer.name + " is still in the game.";
+        } 
+    }
 
     IEnumerator PriestReveal()
     {
-		gameStatusTextObject.text = "Choose another player to reveal their character";
-		yield return StartCoroutine(ChooseOtherPlayer());
-		//Reveal other player
-		gameStatusTextObject.text = chosenOtherPlayer.name + " has a " + chosenOtherPlayer.CurrentState;
+        gameStatusTextObject.text = "Choose another player to reveal their character";
+        yield return StartCoroutine(ChooseOtherPlayer());
+        //Reveal other player
+        gameStatusTextObject.text = chosenOtherPlayer.name + " has a " + chosenOtherPlayer.CurrentState;
     }
 
-	IEnumerator BaronBattle()
+    IEnumerator BaronBattle()
     {
-		gameStatusTextObject.text = "Choose another player you want to battle against";
-		yield return StartCoroutine(ChooseOtherPlayer());
-		if (chosenOtherPlayer.CurrentState < CurrentState)
+        gameStatusTextObject.text = "Choose another player you want to battle against";
+        yield return StartCoroutine(ChooseOtherPlayer());
+        if (chosenOtherPlayer.CurrentState < CurrentState)
         {
             //success
-			gameStatusTextObject.text = "You beat " + chosenOtherPlayer.name + "'s " + chosenOtherPlayer.CurrentState + " in battle";
+            gameStatusTextObject.text = "You beat " + chosenOtherPlayer.name + "'s " + chosenOtherPlayer.CurrentState + " in battle";
             chosenOtherPlayer.Die();
         }
-		else if (chosenOtherPlayer.CurrentState > CurrentState)
+        else if (chosenOtherPlayer.CurrentState > CurrentState)
         {
             //fail
-			gameStatusTextObject.text = chosenOtherPlayer.name + " has a " + chosenOtherPlayer.CurrentState + ". You died in battle";
+            gameStatusTextObject.text = chosenOtherPlayer.name + " has a " + chosenOtherPlayer.CurrentState + ". You died in battle";
             Die();
         }
         else
         {
             //tie
-			gameStatusTextObject.text = chosenOtherPlayer.name + " has a " + chosenOtherPlayer.CurrentState + ". You two tied";
-		}
+            gameStatusTextObject.text = chosenOtherPlayer.name + " has a " + chosenOtherPlayer.CurrentState + ". You two tied";
+        }
     }
 
-	IEnumerator PrinceForceDiscard()
+    IEnumerator PrinceForceDiscard()
     {
-		gameStatusTextObject.text = "Choose another player you want to force to change characters";
-		AllowChooseSelf = true;
-		yield return StartCoroutine(ChooseOtherPlayer());
-		AllowChooseSelf = false;
-		chosenOtherPlayer.SetState(NextStateInDeck); // new state for other player
-		UsedNextState = true; // used to let Game Master know you used the next card
+        gameStatusTextObject.text = "Choose another player you want to force to change characters";
+        AllowChooseSelf = true;
+        yield return StartCoroutine(ChooseOtherPlayer());
+        AllowChooseSelf = false;
+        chosenOtherPlayer.SetState(NextStateInDeck); // new state for other player
+        UsedNextState = true; // used to let Game Master know you used the next card
     }
 
-	IEnumerator KingTradeHands()
+    IEnumerator KingTradeHands()
     {
-		gameStatusTextObject.text = "Choose another player you want to switch characters with";
-		yield return StartCoroutine(ChooseOtherPlayer());
+        gameStatusTextObject.text = "Choose another player you want to switch characters with";
+        yield return StartCoroutine(ChooseOtherPlayer());
         State temp = CurrentState;
-		this.SetState(chosenOtherPlayer.CurrentState);
-		chosenOtherPlayer.SetState(temp);
+        this.SetState(chosenOtherPlayer.CurrentState);
+        chosenOtherPlayer.SetState(temp);
     }
     #endregion
 
     #region Vive Controller Methods
     public void OnGlobalTriggerPressDown(VREventData eventData)
     {
-		if (eventData.currentRaycast == null)
-			return;
+        if (eventData.currentRaycast == null)
+            return;
 
-		if (IsChoosingOtherPlayer) {
-			PlayerController otherPlayerController = eventData.currentRaycast.GetComponent<PlayerController> ();
-			Debug.Log ("Pointing at " + otherPlayerController);
-			if (otherPlayerController != null && !otherPlayerController.Immune) {
-				if (AllowChooseSelf || otherPlayerController != this ) {
-					chosenOtherPlayer = otherPlayerController;
-				}
-			}
-		} else if (IsChoosingOwnState || IsChoosingMenuState) {
-			
-			StateController otherStateController = eventData.currentRaycast.GetComponent<StateController> ();
-			Debug.Log ("Pointing at state: " + otherStateController);
-			if (otherStateController != null) {
-				chosenStateController = otherStateController;
+        if (IsChoosingOtherPlayer) {
+            PlayerController otherPlayerController = eventData.currentRaycast.GetComponent<PlayerController> ();
+            Debug.Log ("Pointing at " + otherPlayerController);
+            if (otherPlayerController != null && !otherPlayerController.Immune) {
+                if (AllowChooseSelf || otherPlayerController != this ) {
+                    chosenOtherPlayer = otherPlayerController;
+                }
+            }
+        } else if (IsChoosingOwnState || IsChoosingMenuState) {
+            
+            StateController otherStateController = eventData.currentRaycast.GetComponent<StateController> ();
+            Debug.Log ("Pointing at state: " + otherStateController);
+            if (otherStateController != null) {
+                chosenStateController = otherStateController;
 
-			}
-		}
+            }
+        }
     }
     #endregion
 }
